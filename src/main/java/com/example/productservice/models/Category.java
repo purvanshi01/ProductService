db3fdd0ffad411924dc4d0dc80a08815491127d6
purvanshi01 <purvanshiyadav@Purvanshis-MacBook-Air.local>
2024-04-26 08:59:01 +0530

    Databases table creation and cardinality mapping

--- src/main/java/com/example/productservice/models/Category.java
+++ src/main/java/com/example/productservice/models/Category.java
@@ -1,11 +1,15 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Category {
-    private long id;
+@Entity
+public class Category extends BaseModel {
+    // @Id
+    // private long id;
     private String title;
 }
--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,14 +1,20 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private long id;
+@Entity
+public class Product extends BaseModel {
+    // @Id
+    // private long id;
     private String title;
     private double price;
+    @ManyToOne
     private Category category;
     private String description;
     private String image;
